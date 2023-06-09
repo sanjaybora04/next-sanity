137707493566b8689491ad984f63389deec53550
Sanjay Bora <sanjaybora380@gmail.com>
2023-06-09 23:31:09 +0530

    commit

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -16,6 +16,9 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
+      <head>
+        <title></title>
+      </head>
       <body className="max-w-3xl mx-auto py-10">
         <header className="flex items-center justify-between">
           <Link href="/" className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent text-lg font-bold">Kapehe</Link>
@@ -28,6 +31,7 @@ export default async function RootLayout({
 
         </header>
         <main className="py-20">{children}</main>
+        <footer>this is footer</footer>
       </body>
     </html>
   )
