7e1034f34d06b9a7422e15ea70474c70642703cd
Sanjay Bora <sanjaybora380@gmail.com>
2024-06-09 14:19:02 +0530

    commit

--- app/(site)/blog/[blog]/page-reader.tsx
+++ app/(site)/blog/[blog]/page-reader.tsx
@@ -1,3 +1,8 @@
 'use client'
-import { PageReader } from "react-page-reader"
+import dynamic from 'next/dynamic';
+const PageReader = dynamic(() => import('react-page-reader').then(mod => mod.PageReader), {
+    // Specify options if needed, such as loading indicator, SSR behavior, etc.
+    loading: () => <p>Loading PageReader</p>,
+    ssr: false // Ensures the component is rendered on the client-side only
+  });
 export default PageReader
