f699b60511f044a84ed5537b411a469dcec5e53c
Sanjay Bora <sanjaybora380@gmail.com>
2024-06-13 21:32:03 +0530

    commit

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,6 +1,7 @@
 import { getProjects } from "@/sanity/sanity-utils";
 import { Metadata } from "next";
 import Link from "next/link"
+import { CloudIcon, GlobeAltIcon, ServerStackIcon } from '@heroicons/react/24/outline'
 
 export const metadata: Metadata = {
   title: "Sanjay Bora | Full Stack Developer",
@@ -98,15 +99,7 @@ export default async function Home() {
               <div className="m-5 text-center">
                 <div className="transition-colors duration-400 hover:text-indigo-400">
                   <div className="p-3 flex justify-center w-full">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      className="w-8 h-8"
-                    >
-                      <path d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418" />
-                    </svg>
+                    <GlobeAltIcon className="w-8 h-8"/>
                   </div>
                   <a>
                     <h4 className="p-3 text-xl font-semibold">Frontend Development</h4>
@@ -121,15 +114,7 @@ export default async function Home() {
               <div className="m-5 text-center">
                 <div className="transition-colors duration-400 hover:text-indigo-400">
                   <div className="p-3 flex justify-center w-full">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      className="w-8 h-8"
-                    >
-                      <path d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5" />
-                    </svg>
+                    <ServerStackIcon className="w-8 h-8"/>
                   </div>
                   <a>
                     <h4 className="p-3 text-xl font-semibold">Backend Development</h4>
@@ -144,16 +129,7 @@ export default async function Home() {
               <div className="m-5 text-center">
                 <div className="transition-colors duration-400 hover:text-indigo-400">
                   <div className="p-3 flex justify-center w-full">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      className="w-8 h-8"
-                    >
-                      <path d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
-                      <path d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                    </svg>
+                    <CloudIcon className="w-8 h-8" />
                   </div>
                   <a>
                     <h4 className="p-3 text-xl font-semibold">Cloud Computing</h4>
