0a4248c25f4817d845ae880d990341dc9939a28d
Sanjay Bora <sanjaybora380@gmail.com>
2024-07-28 17:53:38 +0530

    commit

--- src/app/(site)/blog/[blog]/giscus.tsx
+++ src/app/(site)/blog/[blog]/giscus.tsx
@@ -1,4 +1,10 @@
 'use client'
-import Giscus from '@giscus/react'
+// import Giscus from '@giscus/react'
+import dynamic from 'next/dynamic'
+
+const Giscus = dynamic(() => import('@giscus/react').then(mod => mod), {
+    loading: () => <p>Loading Giscus...</p>,
+    ssr: false
+})
 
 export default Giscus
