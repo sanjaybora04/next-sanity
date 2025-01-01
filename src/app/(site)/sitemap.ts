56b48017aa86850b6d80b0d3270a07c3de3571cb
sanjaybora04 <sanjaybora380@gmail.com>
2025-01-01 17:31:03 +0530

    commit

--- src/app/(site)/sitemap.ts
+++ src/app/(site)/sitemap.ts
@@ -1,6 +1,7 @@
 
-import categories from '@/sanity/config/categories'
-import { getBlogs } from '@/sanity/sanity-utils'
+import Routes from '@/lib/routes'
+import categories from '@/lib/sanity/config/categories'
+import { getBlogs } from '@/lib/sanity/sanity-utils'
 import { MetadataRoute } from 'next'
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
@@ -8,31 +9,31 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 
     const sitemap: MetadataRoute.Sitemap = [
         {
-            url: `${process.env.NEXT_PUBLIC_SITE_URL!}`,
+            url: `${process.env.NEXT_PUBLIC_SITE_URL!}${Routes.home}`,
             lastModified: new Date(),
             changeFrequency: 'weekly',
             priority: 1.0,
         },
         {
-            url: `${process.env.NEXT_PUBLIC_SITE_URL!}/about`,
+            url: `${process.env.NEXT_PUBLIC_SITE_URL!}${Routes.about}`,
             lastModified: new Date(),
             changeFrequency: 'monthly',
             priority: 0.8,
         },
         {
-            url: `${process.env.NEXT_PUBLIC_SITE_URL!}/contact`,
+            url: `${process.env.NEXT_PUBLIC_SITE_URL!}${Routes.projects}`,
             lastModified: new Date(),
             changeFrequency: 'monthly',
             priority: 0.8,
         },
         {
-            url: `${process.env.NEXT_PUBLIC_SITE_URL!}/projects`,
+            url: `${process.env.NEXT_PUBLIC_SITE_URL!}${Routes.blog}`,
             lastModified: new Date(),
             changeFrequency: 'monthly',
             priority: 0.8,
         },
         {
-            url: `${process.env.NEXT_PUBLIC_SITE_URL!}/blog`,
+            url: `${process.env.NEXT_PUBLIC_SITE_URL!}${Routes.contact}`,
             lastModified: new Date(),
             changeFrequency: 'daily',
             priority: 1,
@@ -41,7 +42,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 
     categories.forEach((category) => {
         sitemap.push({
-            url: `${process.env.NEXT_PUBLIC_SITE_URL!}/blog?category=${category.value}`,
+            url: `${process.env.NEXT_PUBLIC_SITE_URL!}${Routes.blog}?category=${category.value}`,
             lastModified: new Date(),
             changeFrequency: 'daily',
             priority: 0.9,
@@ -50,7 +51,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 
     blogs.forEach((blog) => {
         sitemap.push({
-            url: `${process.env.NEXT_PUBLIC_SITE_URL!}/blog/${blog.slug}`,
+            url: `${process.env.NEXT_PUBLIC_SITE_URL!}${Routes.blogPost(blog.slug)}`,
             lastModified: new Date(),
             changeFrequency: 'weekly',
             priority: 0.8,
--- src/sanity/schemas/index.ts
+++
@@ -1,6 +0,0 @@
-import blog from "./blog-schema"
-import project from "./project-schema"
-
-const schemas = [blog,project];
-
-export default schemas;
